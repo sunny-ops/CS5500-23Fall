21de9bb6c7a901593405838f1c97fb91e88b8775
shizhong <shizhong1989@gmail.com>
2023-09-25 11:17:42 -0700

    Modified singleton.py

--- Lab3/src/singleton.py
+++ Lab3/src/singleton.py
@@ -1,8 +1,21 @@
 class Logger:
     _instance = None  # Private class variable to hold the single instance
 
-    def __init__(self):
-        self.messages = []
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Logger, cls).__new__(cls)
+
+            def init_logger(logger_instance):
+                logger_instance.messages = []
+
+            init_logger(cls._instance)
+            print("Logger created exactly once")
+        else:
+            print("logger already created")
+        return cls._instance
+    
+    # def init_Logger(self):
+    #     self.messages = []
 
     def add_message(self, message):
         self.messages.append(message)
@@ -14,7 +27,9 @@ def main():
     for i in range(3):
         logger = Logger()
         logger.add_message(f"Adding message number: {i}")
-
+    print(logger.messages)
+    logger1 = Logger()
+    assert(logger1 == logger)
 
 if __name__ == "__main__":
     main()
