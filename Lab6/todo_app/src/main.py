842951e1fe69b80e02f44b8e64ad9c812ee545ab
shizhong <shizhong1989@gmail.com>
2023-10-21 23:12:17 -0700

    Add 4th get

--- Lab6/todo_app/src/main.py
+++ Lab6/todo_app/src/main.py
@@ -14,9 +14,10 @@ def get_api_path(path_param: str):
 def get_api_query(title:str):
     return {"msg": title}
 
-@app.get("/mybooks/{path_param}")
-def get_api_path(path_param: str):
-    return {"msg": path_param}
+@app.get("/mybooks/{path_param}/")
+def get_api_path_query(path_param: str, title:str):
+    return {"msg": {"path": path_param, "title":title}}
+    # return {"msg": path_param + title}
 
 
 @app.post("/books/create_book")
