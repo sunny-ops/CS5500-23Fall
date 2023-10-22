8eeafb5c6becc17a58d38f89c295833f668932a6
shizhong <shizhong1989@gmail.com>
2023-10-21 23:49:40 -0700

    Task0 finished

--- Lab6/todo_app/src/main.py
+++ Lab6/todo_app/src/main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI, Body
 
 app = FastAPI()
 
+books_db = {
+    1: {"title": "Book 1", "author": "Author 1"},
+    2: {"title": "Book 2", "author": "Author 2"},
+}
+
 @app.get("/api")
 def get_api():
     return {"msg":"hello_worldAAA"}
@@ -24,5 +29,23 @@ def get_api_path_query(path_param: str, query_param:str):
 def first_apiV4(new_book=Body()):
     print(new_book)
     return {"msg": new_book}
+
+@app.put("/books/update_book/{book_id}")
+def update_book(book_id: int, book_data=Body()):
+    if book_id not in books_db:
+        raise HTTPException(status_code=404, detail="Book not Found!")
+    # updated_book = book_data.dict()
+    books_db[book_id] = book_data
+    print(book_data)
+    return {"meg": book_data}
+
+@app.delete("/books/delete_book/{book_id}")
+def delete_book(book_id: int):
+    if book_id not in books_db:
+        raise HTTPException(status_code=404, detail="Book not Found!")
+    book_data = books_db[book_id]
+    del books_db[book_id]
+    print(book_data)
+    return {"meg": book_data}
 
 
