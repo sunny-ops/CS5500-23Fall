ea7ce18092b1fdb34bb677a8d1fcea6ec73b4bae
shizhong <shizhong1989@gmail.com>
2023-10-21 23:04:24 -0700

    Lab6 begin

---
+++ Lab6/todo_app/src/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Body
+
+app = FastAPI()
+
+@app.get("/api")
+def get_api():
+    return {"msg":"hello_worldAAA"}
+
+@app.get("/books/{path_param}")
+def get_api_path(path_param: str):
+    return {"msg": path_param}
+
+@app.get("/books/")
+def get_api_query(title:str):
+    return {"msg": title}
+
+@app.get("/mybooks/{path_param}")
+def get_api_path(path_param: str):
+    return {"msg": path_param}
+
+
+@app.post("/books/create_book")
+def first_apiV4(new_book=Body()):
+    print(new_book)
+    return {"msg": new_book}
+
+
